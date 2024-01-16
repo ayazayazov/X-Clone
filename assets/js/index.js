992d960a5a80a8b879d57960df58f27f8f380f52
ayazayazov <ayazayazov00@gmail.com>
2024-01-17 03:23:39 +0400

    createPost promise created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-async function showPosts (){
+async function showPosts(){
     try{
         const response = await fetch("https://blog-api-t6u0.onrender.com/posts", {
             method: "GET",
@@ -12,3 +12,21 @@ async function showPosts (){
     }
 }
 // showPosts();
+
+async function createPost(form){
+    try {
+        const response = await fetch('https://blog-api-t6u0.onrender.com/posts', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(form)
+        })
+
+        const data = await response.json();
+        console.log(data);
+        return data
+    } catch (err) {
+        console.log('err', err);
+    }
+}
+
+createPost({title: 'Jimmy McGill', body: 'API test create'})
