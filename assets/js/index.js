a15c028adc1339a522537fb62b0e38be4793e7bb
ayazayazov <ayazayazov00@gmail.com>
2024-01-17 03:35:06 +0400

    uptadePost promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,6 @@ async function showPosts(){
         console.log("err", err);
     }
 }
-// showPosts();
 
 async function createPost(form){
     try {
@@ -20,13 +19,32 @@ async function createPost(form){
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(form)
         })
-
+        
         const data = await response.json();
         console.log(data);
-        return data
+        return data;
     } catch (err) {
         console.log('err', err);
     }
 }
 
-createPost({title: 'Jimmy McGill', body: 'API test create'})
+
+async function updatePost(id, form){
+    try {
+        const response = await fetch(`https://blog-api-t6u0.onrender.com/posts/${id}`, {
+            method: 'PUT',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(form)
+        })
+        
+        const data = await response.json();
+        console.log((data));
+        return data;
+    } catch (err) {
+        console.log('err', err);
+    }
+}
+
+// showPosts();
+// createPost({title: 'Jimmy McGill', body: 'API test create'})
+// updatePost(1, {title: "John Doe", body: "Lorem IMPSUM"});
