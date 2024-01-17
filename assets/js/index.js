f57ccb56d215e170a3b5a37883a2d315d9953216
Tofigkh1 <khaniyefdevelop@gmail.com>
2024-01-17 16:44:09 +0400

    fixed repair and dark-mode on

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,6 +16,46 @@ function profileSideOfBtn() {
 }
 
 
+let isDarkMode = false;
+
+function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
+    const body = document.body;
+    const personInfo = document.getElementById('personInfo');
+    const container1 = document.querySelector('.container1');
+    const container2 = document.querySelector('.container2');
+
+    
+
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+        setTimeout(()=>{
+            personInfo.classList.add('dark-mode');
+            container1.classList.add('dark-mode');
+            container2.classList.add('dark-mode');
+        }, 10)
+    } else {
+        body.classList.remove('dark-mode');
+        personInfo.classList.remove('dark-mode');
+        container1.classList.remove('dark-mode');
+        container2.classList.remove('dark-mode');
+    }
+}
+
+
+// let isDarkMode = false;
+
+// function toggleDarkMode() {
+//     const body = document.body;
+//     isDarkMode = !isDarkMode;
+
+//     if (isDarkMode) {
+//         body.classList.add('dark-mode');
+//     } else {
+//         body.classList.remove('dark-mode');
+//     }
+// }
+
 
 // function profileSideOnBtn(){
 //     console.log('click')
